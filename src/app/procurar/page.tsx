60f29ddedf7d9c8ca8dbc47f5157982a5e57cb50
Flavio Francisco <flaviofrancis@msn.com>
2024-07-28 13:50:05 +0200

    Initial commit

---
+++ src/app/procurar/page.tsx
@@ -0,0 +1,5 @@
+export default function SearchPage() {
+    return (        
+            <h1>Procurar</h1>        
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",    
   ],
   theme: {
     extend: {
@@ -13,6 +15,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },      
     },
   },
   plugins: [],
