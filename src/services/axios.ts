386ce3f7515b269760e15a29e1d280f04a4d44c1
Flavio Francisco <flaviofrancis@msn.com>
2024-07-30 21:53:36 +0200

    chore: Update axios.ts to use local API host for development

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_HOST || 'https://api.vagasprajr.com.br';
+const BASE_URL = process.env.REACT_APP_API_HOST || 'http://localhost:3001';
 
 export default axios.create({
   baseURL: BASE_URL,
