d5a9b6bebcd2ea9f0b5f2c5ac525f4003489244a
Flavio Francisco <flaviofrancis@msn.com>
2024-07-30 14:48:21 +0200

    chore: Add Google Analytics to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import MainHeader from "@/components/layout-components/header";
-import MainFooter from "@/components/layout-components/footer";
-
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import MainHeader from '@/components/layout-components/header';
+import GoogleAnalytics from '@/app/GoogleAnalytics';
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Vagas para junior na área de tecnologia da informação (TI) | @vagasprajr",
-  description: "Site com vagas para junior, estágios; trainees na área de tecnologia da informação (TI)",
+  title: 'Vagas para junior na área de tecnologia da informação (TI) | @vagasprajr',
+  description: 'Site com vagas para junior, estágios; trainees na área de tecnologia da informação (TI)',
 };
 
 export default function RootLayout({
@@ -17,12 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">           
-      <body className={inter.className}>        
+    <html lang="en">
+      <GoogleAnalytics />
+      <body className={inter.className}>
         <MainHeader />
-        {children}  
+        {children}
         {/* <MainFooter/>       */}
-      </body>      
+      </body>
     </html>
   );
 }
