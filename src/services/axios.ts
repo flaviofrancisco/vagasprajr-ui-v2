044360ec72b4f1db0214807d0dfda0dd8d6b96ec
Flavio Francisco <flaviofrancis@msn.com>
2024-07-30 14:40:39 +0200

    chore: Update axios.ts to use HTTPS for API requests

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_HOST || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_API_HOST || 'https://api.vagasprajr.com.br';
 
 export default axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
