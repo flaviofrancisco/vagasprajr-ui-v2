33e9469cf5068592299d46740ea4aafcad56f28f
Flavio Francisco <flaviofrancis@msn.com>
2024-08-17 16:50:12 +0200

    refactor: Adjust page container height to fill the screen properly

--- src/components/layout-components/footer.tsx
+++ src/components/layout-components/footer.tsx
@@ -34,9 +34,14 @@ export default function MainFooter() {
                     Github
                   </a>
                 </li>
-                <li>
+                <li className="mb-4">
                   <a href="https://discord.gg/dyShHDrwKC" target="_blank" rel="noopener noreferrer" className="hover:underline">
                     Discord
+                  </a>
+                </li>
+                <li>
+                  <a href="https://t.me/vagasprajr" target="_blank" rel="noopener noreferrer" className="hover:underline">
+                    Telegram
                   </a>
                 </li>
               </ul>
@@ -69,6 +74,12 @@ export default function MainFooter() {
                 <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd" />
               </svg>
               <span className="sr-only">Facebook</span>
+            </a>
+            <a href="https://t.me/vagasprajr" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+              <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 19">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
+              </svg>
+              <span className="sr-only">Telegram</span>
             </a>
             <a href="https://discord.gg/dyShHDrwKC" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
