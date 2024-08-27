5c052e9e765ec5381e46a4b6a8aab64cf6d4396b
Flavio Francisco <flaviofrancis@msn.com>
2024-08-27 10:18:41 +0200

    chore: Enable trailing slash in next.config for better URL structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   images: {
     domains: ['m.media-amazon.com'],
   },
--- src/app/v/[code]/page.tsx
+++ src/app/v/[code]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { getJob } from '@/services/jobs/jobs.service';
 import { useAppDispatch } from '@/services/store';
-import { useParams } from 'next/navigation';
-import { useEffect } from 'react';
+import Link from 'next/link';
+import { useParams, useRouter, redirect, permanentRedirect } from 'next/navigation';
+import { use, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const JobDetailPage: React.FC = () => {
+  const router = useRouter();
   const dispatch = useAppDispatch();
   const params = useParams();
   const code = Array.isArray(params.code) ? params.code[0] : params.code;
@@ -16,19 +18,36 @@ const JobDetailPage: React.FC = () => {
     if (!code) {
       return;
     }
-    dispatch(getJob({ code }));
-  }, [dispatch, code]);
+    dispatch(getJob({ code })).then(() => {
+      router.push(`/v/${code}`);
+    });
+  }, [dispatch, code, router]);
+
+  const getBrFormatedDate = (date: string) => {
+    if (date === '') {
+      return '';
+    }
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString('pt-BR');
+  };
+
+  const formatDescription = (description: string) => {
+    return description.replace(/\n/g, '<br />');
+  };
 
   return (
     <main className="flex-grow">
-      <div className="container mx-auto">
+      <div className="container mx-auto p-10">
         {status === 'loading' && <p>Loading...</p>}
         <h1 className="text-3xl font-bold mt-10 mb-4">{jobView.title}</h1>
         <h2 className="text-xl font-bold mb-4">{jobView.company_name}</h2>
-        <p className="text-lg mb-4">{jobView.location}</p>
+        <Link className="text-lg mb-4 mt-4 font-bold" href={jobView.url} target="_blank" rel="noopener noreferrer">
+          {jobView.url}
+        </Link>
+        <p className="text-lg mt-4 mb-4">{jobView.location}</p>
         <p className="text-lg mb-4">{jobView.salary}</p>
-        <p className="text-lg mb-4">{jobView.created_at}</p>
-        <p className="text-lg mb-4">{jobView.description}</p>
+        <p className="text-lg mb-4">{getBrFormatedDate(jobView.created_at)}</p>
+        <div className="text-lg mb-4" dangerouslySetInnerHTML={{ __html: formatDescription(jobView.description) }} />
       </div>
     </main>
   );
