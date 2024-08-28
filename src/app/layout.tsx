7cb1a7ebd50c4ff9b0c4e48a61e074699d3b4e21
Flavio Francisco <flaviofrancis@msn.com>
2024-08-28 15:09:51 +0200

    refactor: Update Open Graph meta tags for better social media sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>@vagasprajr - Seu site de vagas para juniores; estágio; trainee e pessoas iniciantes na área da tecnologia da informação.</title>
+        <meta property="og:title" content="@vagasprajr" />
+        <meta property="og:description" content="Site com vagas para juniores; estágio; trainee e pessoas iniciantes na área da tecnologia da informação." />
+        <meta property="og:image" content="https://vagasprajr.com.br/_next/image/?url=%2F_next%2Fstatic%2Fmedia%2Flogo.51d0c4db.png&w=96&q=75" />
+        <meta property="og:url" content="https://vagasprajr.com.br/" />
+        <meta property="og:type" content="website" />
+      </head>
       <body className={`${inter.className} flex flex-col min-h-full`}>
         <AuthProvider>
           <Provider store={store}>
