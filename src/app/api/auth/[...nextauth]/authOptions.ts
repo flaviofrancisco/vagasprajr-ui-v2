2c513e466a5087e4c18a66fa988520e0598d43a5
Flavio Francisco <flaviofrancis@msn.com>
2024-09-03 23:12:46 +0200

    refactor: Update Dockerfile and docker-compose.yaml for Google OAuth
    - Renamed NEXT_PUBLIC_OOGLE_CLIENT_ID to NEXT_PUBLIC_GOOGLE_CLIENT_ID in Dockerfile and docker-compose.yaml
    - Updated NEXT_PUBLIC_OOGLE_CLIENT_ID to NEXT_PUBLIC_GOOGLE_CLIENT_ID in authOptions.ts

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -7,7 +7,7 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_OOGLE_CLIENT_ID ?? '',
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
