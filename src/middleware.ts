e8682fc253ad0f4becaba4c650ce0dd11ceddacf
Flavio Francisco <flaviofrancis@msn.com>
2024-09-04 00:53:56 +0200

    refactor: Update admin authentication code in middleware
    The code changes in `middleware.ts` refactor the admin authentication logic. The previous code was commented out and replaced with updated code that makes a POST request to `/auth/authorize` with the necessary headers and credentials. If the response status is not 200 or the `isAuthorized` property in the response data is falsy, the user is redirected to the login page. Otherwise, the user is authorized and the request is allowed to proceed. Additionally, error handling was added to catch any errors during the authentication process and redirect the user to the login page.

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,32 +37,32 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  // let result = await axiosPrivate.post(
-  //   '/auth/authorize',
-  //   { roles: [ADMIN_ROLE] },
-  //   {
-  //     withCredentials: true,
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${cookie?.value}`,
-  //     },
-  //   }
-  // );
+  try {
+    let result = await axiosPrivate.post(
+      '/auth/authorize',
+      { roles: [ADMIN_ROLE] },
+      {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${cookie?.value}`,
+        },
+      }
+    );
 
-  // if (result.status !== 200) {
-  //   console.log('Token is invalid for admin users, redirecting to login.');
-  //   url.pathname = '/auth/login';
-  //   return NextResponse.redirect(url);
-  // }
+    if (result.status !== 200 || !result?.data?.isAuthorized) {
+      console.log('Token is invalid for admin users, redirecting to login.');
+      url.pathname = '/auth/login';
+      return NextResponse.redirect(url);
+    }
 
-  // if (!result?.data?.isAuthorized) {
-  //   console.log('Token is valid for admin users, proceeding with request.');
-  //   url.pathname = '/auth/login';
-  //   return NextResponse.redirect(url);
-  // }
-
-  console.log('Token is valid for authenticated users, proceeding with request.');
-  return NextResponse.next();
+    console.log('Token is valid for admin users, proceeding with request.');
+    return NextResponse.next();
+  } catch (error) {
+    console.log('Error during admin authorization, redirecting to login.');
+    url.pathname = '/auth/login';
+    return NextResponse.redirect(url);
+  }
 }
 
 function isAdminRoute(pathname: string): boolean {
